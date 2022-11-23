41c132cf62a9a61a6511421e556100275c2b546b
zverolen <zverolen@gmail.com>
2022-11-23 16:40:09 +0100

    Fixes the singup section structure

--- source/js/script.js
+++ source/js/script.js
@@ -1,27 +1,21 @@
 const signupForm = document.getElementById('js-signupForm');
 const successMessage = document.getElementById('js-successMessage');
+const buttons = document.querySelectorAll('button');
 const signupButton = document.getElementById('js-signupButton');
-const signupButtonMessage = document.getElementById('js-signupButtonMessage');
-const successButtonMessage = document.getElementById('js-successButtonMessage');
-const signupButtonLabel = 'submit form';
-const successButtonLabel = 'go back';
+const successButton = document.getElementById('js-successButton');
 
-const toggleSignupView = function(e) {
-  e.preventDefault();
+const toggleSignupView = function(event) {
+  event.preventDefault();
   const initialAriaHiddenValue = signupForm.getAttribute('aria-hidden');
   const convertedAriaHiddenValue = (initialAriaHiddenValue === 'true');
   const reversedAriaHiddenValue = (!convertedAriaHiddenValue).toString();
   
   signupForm.setAttribute('aria-hidden', reversedAriaHiddenValue);
-  signupButtonMessage.setAttribute('aria-hidden', reversedAriaHiddenValue);
+  signupButton.setAttribute('aria-hidden', reversedAriaHiddenValue);
   successMessage.setAttribute('aria-hidden', initialAriaHiddenValue);
-  successButtonMessage.setAttribute('aria-hidden', initialAriaHiddenValue);
-
-  if (convertedAriaHiddenValue === true) {
-    e.target.setAttribute('aria-label', successButtonLabel);
-  } else {
-    e.target.setAttribute('aria-label', signupButtonLabel);
-  }
+  successButton.setAttribute('aria-hidden', initialAriaHiddenValue);
 };
 
-signupButton.addEventListener('click', toggleSignupView);
+buttons.forEach((button) => {
+  button.addEventListener('click', toggleSignupView); 
+});
